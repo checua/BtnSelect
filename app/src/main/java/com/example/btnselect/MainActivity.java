72efe6c50083ae59b8bca3749af16374777e7334
checua <profesor76@hotmail.com>
2021-05-13 18:23:53 -0500

    Initial commit Primera versión en Github 1.0 13/05/2021

---
+++ app/src/main/java/com/example/btnselect/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.btnselect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int n1 = (int) (Math.random()*100);
+                if (n1 >= 50) {
+                    Toast.makeText(getApplicationContext(), "Acertaste", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Fallaste", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+        final Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int n1 = (int) (Math.random()*100);
+                if (n1 >= 50) {
+                    Toast.makeText(getApplicationContext(), "Acertaste", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Fallaste", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+    }
+
+}
