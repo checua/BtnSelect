cdd7bcbb7d5cbc35fd4817bf12b42ea078f2ccc0
checua <profesor76@hotmail.com>
2021-05-22 13:55:48 -0500

    DOs

---
+++ app/src/main/java/com/example/btnselect/Pop.java
@@ -0,0 +1,14 @@
+package com.example.btnselect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop);
+    }
+}
