197bdc4eed32d24337efc95e5b501aba1c302a80
checua <profesor76@hotmail.com>
2021-05-22 14:20:26 -0500

    DOs

--- app/src/main/java/com/example/btnselect/Pop.java
+++ app/src/main/java/com/example/btnselect/Pop.java
@@ -1,11 +1,8 @@
 package com.example.btnselect;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
 public class Pop extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
